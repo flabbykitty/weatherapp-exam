f84c93cda886f899d68743497cfe87fbfda9b274
Jasmine Silfver <jasmine.silfver@gmail.com>
2020-03-02 11:15:32 +0100

    Added error handling, and had a duh moment with report...

--- src/App.js
+++ src/App.js
@@ -7,27 +7,25 @@ class App extends React.Component {
 	state = {
 		errorMessage: false,
 		report: null,
-		weather: {
-			city: '',
-			temp: '',
-			humidity: ''
-		}
 	}
 
 	handleSearch = (city) => {
 		axios.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=a9f6719e37f20890ebff5d91724dec1f')
 			.then(response => {
 				this.setState({
-					report: true,
-					weather: {
-						city: city,
+					errorMessage: false,
+					report: {
+						city,
 						temp: Math.round(response.data.main.temp * 10 ) / 10,
 						humidity: response.data.main.humidity
 					}
 				})
 			})
-			.catch(err => {
-				console.error(err)
+			.catch((err) => {
+				this.setState({
+					errorMessage: true,
+					report: null
+				})
 			})
 	}
 
@@ -44,7 +42,15 @@ class App extends React.Component {
 					{
 						this.state.report
 						? (
-							<WeatherReport weather={this.state.weather} />
+							<WeatherReport weather={this.state.report} />
+						)
+						: ''
+					}
+
+					{
+						this.state.errorMessage
+						? (
+							<div className="alert alert-warning text-center">That city does not exist</div>
 						)
 						: ''
 					}
